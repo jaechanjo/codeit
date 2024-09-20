4068fb67a2350e79bfd961a7e5f7f96f24db1278
jaechanjo <jjc12223a@gmail.com>
2024-09-20 12:14:25 +0900

    Add get_Median function

--- calculator.py
+++ calculator.py
@@ -7,3 +7,16 @@ def multiply(a, b):
   return a*b
 def divide(a, b):
   return a/b
+def get_median(x):
+    # 리스트를 오름차순으로 정렬
+    x = sorted(x)
+    
+    # 리스트의 길이를 계산
+    n = len(x)
+    
+    # 길이가 홀수일 경우, 가운데 값 반환
+    if n % 2 == 1:
+        return x[n // 2]
+    # 길이가 짝수일 경우, 가운데 두 값의 평균 반환
+    else:
+        return (x[n // 2 - 1] + x[n // 2]) / 2
